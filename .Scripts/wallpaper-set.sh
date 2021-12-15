93dbcb3f2afe0f80a2a57c0e8b2daf51f42e4bea
Mario Calcagno <mario.calcagno03@gmail.com>
2021-12-15 23:15:52 +0100

    inizio migrazione verso spectrwm

--- .Scripts/wallpaper-set.sh
+++ .Scripts/wallpaper-set.sh
@@ -4,9 +4,9 @@ second_monitor_flag=$(xrandr -q | grep " connected" | wc | awk '{ print $1 }')
 
 case "$second_monitor_flag" in
     1)
-	xwallpaper --output eDP-1 --maximize ~/Immagini/Wallpapers/Space_EndeavourOS.png ;;
+	xwallpaper --output eDP-1 --zoom ~/Immagini/Wallpapers/dracula.png ;;
     2)
-	xwallpaper --output eDP-1 --zoom ~/Immagini/Wallpapers/Space_EndeavourOS.png
-	xwallpaper --output HDMI-2 --zoom ~/Immagini/Wallpapers/Space_EndeavourOS.png
+	xwallpaper --output eDP-1 --zoom ~/Immagini/Wallpapers/dracula.png
+	xwallpaper --output HDMI-2 --zoom ~/Immagini/Wallpapers/dracula.png
 	;;
 esac
